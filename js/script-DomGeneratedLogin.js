02410a75951c01ebc5df9e851a2ef570108ebd42
SashaGonchar <nekospoon228@gmail.com>
2023-12-12 13:27:16 +0200

    Changed let to const in variables that will not change

--- js/script-DomGeneratedLogin.js
+++ js/script-DomGeneratedLogin.js
@@ -1,48 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let signInForm = document.createElement("div");
+    const signInForm = document.createElement("div");
     signInForm.className = "sign-in-form";
     document.body.append(signInForm);
 
-    let signInFormContainer = document.createElement("div");
+    const signInFormContainer = document.createElement("div");
     signInFormContainer.className = "sign-in-form__container";
     signInForm.append(signInFormContainer);
 
-    let signInFormLogo = document.createElement("img");
+    const signInFormLogo = document.createElement("img");
     signInFormLogo.className = "sign-in-form__logo";
     signInFormLogo.src="img/Logo.svg";
     signInFormLogo.alt="logo";
     signInFormContainer.append(signInFormLogo);
 
-    let signInFormTitleContainer = document.createElement("div");
+    const signInFormTitleContainer = document.createElement("div");
     signInFormTitleContainer.className = "sign-in-form__title";
     signInFormContainer.append(signInFormTitleContainer);
 
-    let signInFormTitle = document.createElement("p");
+    const signInFormTitle = document.createElement("p");
     signInFormTitle.className = "text text_title";
     signInFormTitle.innerText = "Sign in to your account"
     signInFormTitleContainer.append(signInFormTitle);
 
-    let signInFormNotAMember = document.createElement("p");
+    const signInFormNotAMember = document.createElement("p");
     signInFormNotAMember.className = "text text_smokey";
     signInFormNotAMember.innerText = "Not a member? "
     signInFormTitleContainer.append(signInFormNotAMember);
 
-    let signInFormSignUpLink = document.createElement("a");
+    const signInFormSignUpLink = document.createElement("a");
     signInFormSignUpLink.className = "text text_link";
     signInFormSignUpLink.innerText = "Sign Up "
     signInFormNotAMember.append(signInFormSignUpLink);
 
-    let signInFormEmailLabelContainer = document.createElement("div");
+    const signInFormEmailLabelContainer = document.createElement("div");
     signInFormEmailLabelContainer.className = "sign-in-form__label";
     signInFormContainer.append(signInFormEmailLabelContainer);
 
-    let signInFormEmailLabel = document.createElement("label");
+    const signInFormEmailLabel = document.createElement("label");
     signInFormEmailLabel.innerText = "Email"
     signInFormEmailLabel.className = "text text_smokey";
     signInFormEmailLabel.setAttribute("for", "email");
     signInFormEmailLabelContainer.append(signInFormEmailLabel);
 
-    let signInFormEmailInput = document.createElement("input");
+    const signInFormEmailInput = document.createElement("input");
     signInFormEmailInput.className = "sign-in-form__input text text_gray";
     signInFormEmailInput.setAttribute("id", "email");
     signInFormEmailInput.setAttribute("type", "email");
@@ -50,26 +50,26 @@ document.addEventListener("DOMContentLoaded", function() {
     signInFormEmailInput.setAttribute("placeholder", "Your Email");
     signInFormEmailLabelContainer.append(signInFormEmailInput);
 
-    let signInFormPasswordLabelContainer = document.createElement("div");
+    const signInFormPasswordLabelContainer = document.createElement("div");
     signInFormPasswordLabelContainer.className = "sign-in-form__label";
     signInFormContainer.append(signInFormPasswordLabelContainer);
 
-    let rowContainer = document.createElement("div");
+    const rowContainer = document.createElement("div");
     rowContainer.className = "row-container";
     signInFormPasswordLabelContainer.append(rowContainer);
 
-    let signInFormPasswordLabel = document.createElement("label");
+    const signInFormPasswordLabel = document.createElement("label");
     signInFormPasswordLabel.innerText = "Password";
     signInFormPasswordLabel.setAttribute("for", "password");
     signInFormPasswordLabel.className = "text text_smokey";
     rowContainer.append(signInFormPasswordLabel);
 
-    let signInFormForgotPassword = document.createElement("a");
+    const signInFormForgotPassword = document.createElement("a");
     signInFormForgotPassword.className = "text text_link";
     signInFormForgotPassword.innerText = "Forgot Password?"
     rowContainer.append(signInFormForgotPassword);
 
-    let signInFormPasswordInput = document.createElement("input");
+    const signInFormPasswordInput = document.createElement("input");
     signInFormPasswordInput.className = "sign-in-form__input text text_gray";
     signInFormPasswordInput.setAttribute("id", "password");
     signInFormPasswordInput.setAttribute("type", "password");
@@ -77,24 +77,24 @@ document.addEventListener("DOMContentLoaded", function() {
     signInFormPasswordInput.setAttribute("placeholder", "Type your password here");
     signInFormPasswordLabelContainer.append(signInFormPasswordInput);
 
-    let signInFormCheckboxContainer = document.createElement("div");
+    const signInFormCheckboxContainer = document.createElement("div");
     signInFormCheckboxContainer.className = "sign-in-form__checkbox";
     signInFormContainer.append(signInFormCheckboxContainer);
 
-    let signInFormCheckboxInput = document.createElement("input");
+    const signInFormCheckboxInput = document.createElement("input");
     signInFormCheckboxInput.className = "custom_checkbox";
     signInFormCheckboxInput.setAttribute("id", "remember");
     signInFormCheckboxInput.setAttribute("type", "checkbox");
     signInFormCheckboxInput.setAttribute("name", "remember");
     signInFormCheckboxContainer.append(signInFormCheckboxInput);
 
-    let signInFormCheckboxLabel = document.createElement("label");
+    const signInFormCheckboxLabel = document.createElement("label");
     signInFormCheckboxLabel.innerText = "Remember me";
     signInFormCheckboxLabel.setAttribute("for", "remember");
     signInFormCheckboxLabel.className = "text text_small";
     signInFormCheckboxContainer.append(signInFormCheckboxLabel);
 
-    let signInFormButton = document.createElement("button");
+    const signInFormButton = document.createElement("button");
     signInFormButton.innerText = "Sign in";
     signInFormButton.className = "sign-in-form__button text";
     signInFormContainer.append(signInFormButton);
